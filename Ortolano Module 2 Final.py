87f1cdfc9fe2dafb59c8fc1f6b52c633852cdcbf
Owlswerth <50796650+ortolanoj@users.noreply.github.com>
2022-09-21 20:24:19 -0400

    Add files via upload

--- Ortolano Module 2 Final.py
+++ Ortolano Module 2 Final.py
@@ -1,2 +1,28 @@
-# Display the output
-print("New Python file")
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **John Ortolano**
+
+# Decision Scientist
+
+# *I am interested in data science as I want to continue to lead my company strategically with the most accurate real-time data*
+
+# ### The cell below uses simple mathematics to execute the code 1+1 using Python that returns two
+
+# In[3]:
+
+
+1 + 1
+
+
+# [Coursera] (https://www.coursera.org)
+# ~~HELLO WORLD~~
+# > The rain in Spain falls mainly on the plan
+
+# In[ ]:
+
+
+
+
